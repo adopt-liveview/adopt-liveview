119d9afa6da33705d061842c79e3a662f7b9e055
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-04-03 16:14:32 -0300

    Add hero - WIP

--- lib/curso_web/components/core_components.ex
+++ lib/curso_web/components/core_components.ex
@@ -833,6 +833,43 @@ defmodule CursoWeb.CoreComponents do
     """
   end
 
+  attr :title, :string
+  attr :description, :string
+  attr :image, :string
+
+
+  # WIP
+  def hero(assigns) do
+    ~H"""
+    <div class="overflow-hidden bg-slate-900 dark:-mb-32 dark:mt-[-4.75rem] dark:pb-32 dark:pt-[4.75rem]">
+      <div class="py-16 sm:px-2 lg:relative lg:px-0 lg:py-20">
+        <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 lg:max-w-8xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
+          <div class="relative z-10 md:text-center lg:text-left">
+            <img src={@image}/>
+            <div className="relative">
+              <p className="inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+                <%= @title %>
+              </p>
+              <p className="mt-3 text-2xl tracking-tight text-slate-400">
+                <%= @description %>
+              </p>
+              <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
+                <a href="/" class="rounded-full bg-sky-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500">
+                  Get Started
+                </a>
+
+                <a href="/" class="rounded-full bg-slate-800 py-2 px-4 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400">
+                  View Github
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
   attr :title, :string, default: nil
   attr :section, :string, default: nil
 
