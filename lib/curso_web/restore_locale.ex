fffc89df4d18aefd65df095d8ed39c0ed7cff46a
lubien <lubien1996@gmail.com>
2024-04-08 07:26:18 -0300

    Delete unused case

--- lib/curso_web/restore_locale.ex
+++ lib/curso_web/restore_locale.ex
@@ -4,7 +4,4 @@ defmodule CursoWeb.RestoreLocale do
     Gettext.put_locale(CursoWeb.Gettext, locale)
     {:cont, Phoenix.Component.assign(socket, locale: locale)}
   end
-
-  # catch-all case
-  def on_mount(:default, _params, _session, socket), do: {:cont, socket}
 end
