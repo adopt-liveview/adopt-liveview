5e98dab9bed415ca2be17cb72e5890931d26c4c4
lubien <lubien1996@gmail.com>
2024-04-15 09:53:38 -0300

    wtf sentry, make clear instructions

--- config/runtime.exs
+++ config/runtime.exs
@@ -120,5 +120,5 @@ if config_env() == :prod do
     dsn: System.get_env("SENTRY_DSN"),
     environment_name: :prod,
     enable_source_code_context: true,
-    root_source_code_paths: File.cwd!()
+    root_source_code_paths: [File.cwd!()]
 end
