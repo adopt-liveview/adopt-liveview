aed00bbee6b2d5b73cf14798a38bb3e6ca388cc7
lubien <lubien1996@gmail.com>
2026-05-02 08:23:58 -0300

    update sitemap.exs

--- priv/scripts/sitemap.exs
+++ priv/scripts/sitemap.exs
@@ -8,13 +8,24 @@ config = [
   sitemap_url: "#{domain}/sitemaps"
 ]
 
-Curso.Pages.all_pages()
-|> Stream.map(fn page ->
-  %Sitemapper.URL{
-    loc: "#{domain}/guides/#{page.id}/#{page.language}",
-    lastmod: page.modified_at |> DateTime.to_date()
-  }
-end)
+home_urls =
+  Stream.map(["/", "/en", "/br"], fn path ->
+    %Sitemapper.URL{
+      loc: "#{domain}#{path}",
+      lastmod: Date.utc_today()
+    }
+  end)
+
+guide_urls =
+  Curso.Pages.all_pages()
+  |> Stream.map(fn page ->
+    %Sitemapper.URL{
+      loc: "#{domain}/guides/#{page.id}/#{page.language}",
+      lastmod: page.modified_at |> DateTime.to_date()
+    }
+  end)
+
+Stream.concat(home_urls, guide_urls)
 |> Sitemapper.generate(config)
 |> Sitemapper.persist(config)
 |> Stream.run()
